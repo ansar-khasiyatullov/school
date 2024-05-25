bf2374ec584bf2616d5931472694da3a95664493
intelcore <becar_02@mail.ru>
2024-05-25 18:58:38 +0300

    L20240501

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.Collection;
 import java.util.List;
 
 @Service
 public class AvatarService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(AvatarService.class);
     private final AvatarRepository avatarRepository;
     private final StudentRepository studentRepository;
     private final Path avatarsDir;
@@ -28,6 +30,7 @@ public class AvatarService {
     }
 
     public Avatar save(Long studentId, MultipartFile file) throws IOException {
+        LOGGER.info("Avatar.save was invoked!");
 
         Files.createDirectories(avatarsDir);
 
@@ -48,10 +51,12 @@ public class AvatarService {
     }
 
     public Avatar getById(Long id) {
+        LOGGER.info("Avatar.getById was invoked!");
         return avatarRepository.findById(id).orElse(null);
     }
 
     public List<Avatar> getPage(int pageNumber, int pageSize) {
+        LOGGER.info("Avatar.getPage was invoked!");
         return avatarRepository.findAll(PageRequest.of(pageNumber, pageSize)).toList();
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exceptions.RecordNotFoundException;
 import ru.hogwarts.school.model.Faculty;
@@ -10,6 +12,7 @@ import java.util.Collection;
 
 @Service
 public class StudentService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(StudentService.class);
     private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
@@ -17,14 +20,17 @@ public class StudentService {
     }
 
     public Student createStudent(Student student) {
+        LOGGER.info("Student.create was invoked!");
         return studentRepository.save(student);
     }
 
     public Student findStudent(long id) {
+        LOGGER.info("Student.find was invoked!");
         return studentRepository.findById(id).orElseThrow(RecordNotFoundException::new);
     }
 
     public boolean deleteStudent(long id) {
+        LOGGER.info("Student.delete was invoked!");
         return studentRepository.findById(id).map(entity -> {
             studentRepository.delete(entity);
             return true;
@@ -32,36 +38,44 @@ public class StudentService {
     }
 
     public Student editStudent(Student student) {
+        LOGGER.info("Student.edit was invoked!");
         return studentRepository.findById(student.getId())
                 .map(entity -> studentRepository.save(student))
                 .orElse(null);
     }
 
-    public Collection<Student> getByAgeStudents(int age){
+    public Collection<Student> getByAgeStudents(int age) {
+        LOGGER.info("Student.getByAgeStudents was invoked!");
         return studentRepository.findByAge(age);
     }
 
     public Collection<Student> getByAgeBetween(int min, int max) {
+        LOGGER.info("Student.getByAgeBetween was invoked!");
         return studentRepository.findAllByAgeBetween(min, max);
     }
 
     public Collection<Student> getAllStudents() {
+        LOGGER.info("Student.getAllStudents was invoked!");
         return studentRepository.findAll();
     }
 
-    public Faculty getFacultyByStudent(Long id){
+    public Faculty getFacultyByStudent(Long id) {
+        LOGGER.info("Student.getFacultyByStudent was invoked!");
         return findStudent(id).getFaculty();
     }
 
-    public int getStudentCount(){
+    public int getStudentCount() {
+        LOGGER.info("Student.getStudentCount was invoked!");
         return studentRepository.countStudents();
     }
 
-    public double getAvgAge(){
+    public double getAvgAge() {
+        LOGGER.info("Student.getAvgAge was invoked!");
         return studentRepository.avgAge();
     }
 
-    public Collection<Student> getLastFive(){
+    public Collection<Student> getLastFive() {
+        LOGGER.info("Student.getLastFive was invoked!");
         return studentRepository.getLastFive();
     }
 }
